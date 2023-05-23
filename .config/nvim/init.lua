f3f71b8388ffa5da6334463b0779b3a7f9d5178e
Lem3s <joaovictorlemesfaria@gmail.com>
2023-05-23 11:01:51 -0300

    primeiro commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,7 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+require "user.cmp"
+require "user.lsp"
