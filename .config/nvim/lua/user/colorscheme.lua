5cb985f34a9c62e84c61d5bf1a5bc815db1dc71c
lem3s <joaovictorlemesfaria@gmail.com>
2023-11-07 22:24:33 -0300

    remoção dos arquivos antigos

--- .config/nvim/lua/user/colorscheme.lua
+++
@@ -1,5 +0,0 @@
-vim.o.background = "dark" -- or "light" for light mode
-vim.g.gruvbox_material_background = 'hard'
-vim.g.gruvbox_material_foreground = 'material'
-vim.g.gruvbox_material_transparent_background = '1'
-vim.cmd.colorscheme("gruvbox-material")
